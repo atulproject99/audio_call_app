afbe11eced15e738fb53606eb1d9289546c7d612
Atul maurya <atul@bit01.tech>
2024-06-12 22:54:39 +0530

    initial release

---
+++ lib/call_page.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+
+class CallingPage extends StatelessWidget {
+  const CallingPage({super.key, required this.callId});
+  final String callId;
+  @override
+  Widget build(BuildContext context) {
+    final localUserId = Random().nextInt(9999) + 1000;
+    return Scaffold(
+      body: ZegoUIKitPrebuiltCall(
+          appID:
+              1863576415, // Fill in the appID that you get from ZEGOCLOUD Admin Console.
+          appSign:
+              '50fe36f9373422b3c01375b7f5ca6f438e9bf40fd9cb00830269934c693742df', // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
+          userID: localUserId.toString(),
+          userName: 'User $localUserId',
+          callID: callId,
+          // You can also use groupVideo/groupVoice/oneOnOneVoice to make more types of calls.
+          config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:audio_call_app/call_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter call',
+      debugShowMaterialGrid: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Calling page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Call ID',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Call ID is required';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => CallingPage(
+                        callId: _controller.text,
+                      ),
+                    ));
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
